ebfc8fe787760bc8c2393f065d9aaa2ffe95480b
Lesley Chou <lesleychou339@gmail.com>
2021-06-29 18:24:09 +0900

    fixed

--- bo_train.py
+++ bo_train.py
@@ -1,9 +1,23 @@
 """
 ====== Train the RL agent with BO guided domain randomization ======
 """
-
 import os
+import argparse
 from rl_heuristic_agents import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="BO training script.")
+    parser.add_argument( "--training_param" ,type=str,
+                         required=True ,help='current training param' )
+    parser.add_argument("--training_range", type=float,
+                        required=True, help='current training range')
+    parser.add_argument("--model_path", type=str, required=True,
+                        help='saved model path')
+
+    return parser.parse_args()
+
 
 def main():
     '''
@@ -11,7 +25,7 @@ def main():
     save the trained model to nn_model_path
     '''
 
-    rl_agent = RLAgent(summary_dir='')
+    rl_agent = RLAgent()
     # TODO: update the training_range_input
     rl_agent.training_agent(env_range=1)
 
